980f6c92ba24f9374c977d22ed8089db1b9aea39
Maxime Genet <maxime.genet@kosmos.fr>
2026-01-16 17:52:15 -0500

    Set file manager as default pane

--- frontend/src/pages/cfs/fileManage/fileList/index.vue
+++ frontend/src/pages/cfs/fileManage/fileList/index.vue
@@ -55,7 +55,7 @@ export default {
     return {
       id: '',
       zone: '',
-      activeName: 'cors',
+      activeName: 'file',
       volName: '',
       corsRules: [],
       disabledFile: true,
